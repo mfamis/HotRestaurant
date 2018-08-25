2a3f5f2895862540e56919598b9a88836e243689
Jake Dykstra <jacob.d.dykstra@gmail.com>
2018-08-25 13:44:27 -0700

    small fix

--- server.js
+++ server.js
@@ -106,12 +106,12 @@ app.post('/reserve', function (req, res) {
   if (restaurants.length == 5) {
     waitlist.push(reservation)
     console.log(waitlist);
-    return res.json(true);
+    return res.json(false);
     
   } else {
     restaurants.push(reservation);
     console.log(restaurants);
-    return res.json(false);
+    return res.json(true);
   }
 
   res.json(reservation)
